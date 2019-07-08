01bd0efc18e363c5d9e66dd1268bfc9c2c6ec0d5
Andrej.Feller <a.feller@intabia.ru>
2019-07-08 13:06:52 +0700

    change return value

--- second/src/main/java/com/example/second/demo/second/SecondApplication.java
+++ second/src/main/java/com/example/second/demo/second/SecondApplication.java
@@ -11,7 +11,7 @@ public class SecondApplication {
 
     @GetMapping(path="/main")
     public String get(){
-        return "wow";
+        return "Tarara";
     }
 
     public static void main(String[] args) {
