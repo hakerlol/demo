84276379d906a27c0c8dc02fcd3687454520fa85
Andrej.Feller <a.feller@intabia.ru>
2019-07-08 10:34:01 +0700

    add second demo

---
+++ second/src/main/java/com/example/second/demo/second/SecondApplication.java
@@ -0,0 +1,21 @@
+package com.example.second.demo.second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SecondApplication {
+
+    @GetMapping(path="/main")
+    public String get(){
+        return "55";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondApplication.class, args);
+    }
+
+}
